79fc0bc28a7768ded2cfe86bc6d6830eac788dd3
Liang Ding <d@b3log.org>
2018-07-29 00:17:31 +0800

    :art: 作者信息

--- model/answer.go
+++ model/answer.go
@@ -8,11 +8,13 @@ type Answer struct {
 	Model
 
 	QuestionID  uint64 `sql:"index" json:"questionID"`
-	Votes       int `json:"votes"`
+	Votes       int    `json:"votes"`
 	ContentEnUS string `gorm:"type:mediumtext" json:"contentEnUS"`
 	ContentZhCN string `gorm:"type:mediumtext" json:"contentZhCN"`
 	Path        string `gorm:"type:text" json:"path"`
 	Source      int    `sql:"index" json:"source"`
 	SourceID    string `gorm:"size:255" sql:"index"`
 	SourceURL   string `gorm:"size:255" sql:"index" json:"sourceURL"`
+	AuthorName  string `json:"authorName"`
+	AuthorURL   string `gorm:"size:255" json:"authorURL"`
 }
--- model/question.go
+++ model/question.go
@@ -10,14 +10,16 @@ type Question struct {
 	TitleEnUS   string `gorm:"type:text" json:"titleEnUS"`
 	TitleZhCN   string `gorm:"type:text" json:"titleZhCN"`
 	Tags        string `gorm:"type:text" json:"tags"`
-	Votes       int `json:"votes"`
-	Views       int `json:"views"`
+	Votes       int    `json:"votes"`
+	Views       int    `json:"views"`
 	ContentEnUS string `gorm:"type:mediumtext" json:"contentEnUS"`
 	ContentZhCN string `gorm:"type:mediumtext" json:"contentZhCN"`
 	Path        string `gorm:"type:text" json:"path"`
 	Source      int    `sql:"index" json:"source"`
 	SourceID    string `gorm:"size:255" sql:"index"`
 	SourceURL   string `gorm:"size:255" sql:"index" json:"sourceURL"`
+	AuthorName  string `json:"authorName"`
+	AuthorURL   string `gorm:"size:255" json:"authorURL"`
 }
 
 // Sources.
