4b0f628ab3e7e604727199bca7aca1c3deff6859
Van <lly219@gmail.com>
2018-10-03 22:57:19 +0800

    :art: list

--- view/gulpfile.js
+++ view/gulpfile.js
@@ -2,20 +2,20 @@
  * @file scss tool.
  *
  * @author <a href='http://vanessa.b3log.org'>Liyuan Li</a>
- * @version 0.1.0.1, Apr 8, 2018
+ * @version 0.2.0.0, Oct 3, 2018
  */
 
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 
-gulp.task('sass', function () {
-  return gulp.src('./scss/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-})
+function sassProcess () {
+  return gulp.src('./scss/*.scss').
+    pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).
+    pipe(gulp.dest('./css'))
+}
 
-gulp.task('watch', function () {
-  gulp.watch(['./scss/*.scss'], ['sass'])
-})
+function sassProcessWatch () {
+  gulp.watch('./scss/*.scss', sassProcess)
+}
 
-gulp.task('default', ['sass'])
+gulp.task('watch', gulp.series(sassProcessWatch))
